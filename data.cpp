fac9c31a35dac14708271091d6c33369f4f32f23
ARIBES <axel.ribes@ynov.com>
2021-06-16 17:52:04 +0200

    c++data

---
+++ data.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+Tigre Male = Alimentation{"viande", 12,2}, pontePortée{0}, maturiteSex{6}, gestation{0}, fiReproduction{14}, mortInfantile{0}, esperanceVie{25}, remarques{""}
+Tigre Femelle = Alimentation{"viande", 10,2}, pontePortée{[1,2,3], 20}, maturiteSex{4}, gestation{3}, fiReproduction{14}, mortInfantile{0.33}, esperanceVie{25}, remarques{""}
+
+Aigle Male = Alimentation{"viande", 0.25,10}, pontePortée{0}, maturiteSex{4}, gestation{0}, fiReproduction{14}, mortInfantile{0}, esperanceVie{25}, remarques{"fidèle"}
+Aigle Femelle = Alimentation{"viande", 0.3,10}, pontePortée{2, "Mars"}, maturiteSex{4}, gestation{45}, fiReproduction{14}, mortInfantile{0.5}, esperanceVie{25}, remarques{"fidèle"}
+
+c pas du c++ mais je mets juste les données
